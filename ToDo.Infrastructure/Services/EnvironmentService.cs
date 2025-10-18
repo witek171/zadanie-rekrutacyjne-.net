c1e55d8be04fbe2f91a2bde96d282904989dc6b7
Witold <witold778899@gmail.com>
2025-10-18 17:41:46 +0200

    Add `EnvironmentService` for managing and retrieving environment variables

---
+++ ToDo.Infrastructure/Services/EnvironmentService.cs
@@ -0,0 +1,20 @@
+﻿namespace ToDo.Infrastructure.Services;
+
+public static class EnvironmentService
+{
+	public static string SqlConnectionString => GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+	public static string EnvName => GetEnvironmentVariable("OtherEnvVariable");
+
+	private static string GetEnvironmentVariable(string variable)
+	{
+		string? value = Environment.GetEnvironmentVariable(variable);
+
+		if (string.IsNullOrWhiteSpace(value))
+		{
+			string errorMessage = $"Environment variable '{variable}' not found or is empty";
+			throw new InvalidOperationException(errorMessage);
+		}
+
+		return value;
+	}
+}
