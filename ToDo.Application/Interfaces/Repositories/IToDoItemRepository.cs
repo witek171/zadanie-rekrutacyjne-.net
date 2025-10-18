01ed28874b092abfbdd7da35d0f92d57805c11c8
Witold <witold778899@gmail.com>
2025-10-18 20:21:43 +0200

    Add `IToDoItemRepository` interface and its implementation in Infrastructure

---
+++ ToDo.Application/Interfaces/Repositories/IToDoItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using ToDo.Domain.Models;
+
+namespace ToDo.Application.Interfaces.Repositories;
+
+public interface IToDoItemRepository
+{
+	Task<ToDoItem> GetByIdAsync(Guid id);
+	Task<IEnumerable<ToDoItem>> GetAllAsync();
+	Task AddAsync(ToDoItem toDoItem);
+	Task UpdateAsync(ToDoItem toDoItem);
+	Task DeleteAsync(Guid id);
+	Task<bool> ExistsAsync(Guid id);
+}
---
+++ ToDo.Infrastructure/Repositories/ToDoItemRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Application.Interfaces.Repositories;
+using ToDo.Domain.Models;
+using ToDo.Infrastructure.Data;
+
+namespace ToDo.Infrastructure.Repositories;
+
+public class ToDoItemRepository : IToDoItemRepository
+{
+	private readonly ApplicationDbContext _context;
+
+	public ToDoItemRepository(ApplicationDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<ToDoItem> GetByIdAsync(Guid id)
+		=> (await _context.ToDoItems.FindAsync(id))!;
+
+	public async Task<IEnumerable<ToDoItem>> GetAllAsync()
+		=> await _context.ToDoItems.ToListAsync();
+
+	public async Task AddAsync(ToDoItem item)
+	{
+		await _context.ToDoItems.AddAsync(item);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task UpdateAsync(ToDoItem item)
+	{
+		_context.ToDoItems.Update(item);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task DeleteAsync(Guid id)
+	{
+		ToDoItem item = (await _context.ToDoItems.FindAsync(id))!;
+		_context.ToDoItems.Remove(item);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task<bool> ExistsAsync(Guid id)
+		=> await _context.ToDoItems.AnyAsync(x => x.Id == id);
+}
