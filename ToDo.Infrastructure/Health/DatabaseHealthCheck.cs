5210d8619d798f1d0c7579ee51881a4d4452f8ee
Witold <witold778899@gmail.com>
2025-10-18 17:41:37 +0200

    Add `DatabaseHealthCheck` implementation for database connection monitoring

---
+++ ToDo.Infrastructure/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using Npgsql;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using ToDo.Infrastructure.Services;
+
+namespace ToDo.Infrastructure.Health;
+
+public sealed class DatabaseHealthCheck : IHealthCheck
+{
+	public async Task<HealthCheckResult> CheckHealthAsync(
+		HealthCheckContext context,
+		CancellationToken cancellationToken = new())
+	{
+		try
+		{
+			string connectionString = EnvironmentService.SqlConnectionString;
+
+			await using NpgsqlConnection connection = new(connectionString);
+			await connection.OpenAsync(cancellationToken);
+
+			await using NpgsqlCommand command = new("SELECT 1", connection);
+			await command.ExecuteScalarAsync(cancellationToken);
+
+			return HealthCheckResult.Healthy();
+		}
+		catch (Exception e)
+		{
+			return HealthCheckResult.Unhealthy(exception: e);
+		}
+	}
+}
