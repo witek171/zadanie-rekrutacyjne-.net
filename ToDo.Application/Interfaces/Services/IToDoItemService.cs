699a17be58868c449d7d22e8fedebdc2da14d724
Witold <witold778899@gmail.com>
2025-10-19 11:01:38 +0200

    Add `IToDoItemService` interface and its implementation

---
+++ ToDo.Application/Interfaces/Services/IToDoItemService.cs
@@ -0,0 +1,21 @@
+﻿using ToDo.Domain.Models;
+using ToDo.Domain.Models.Enums;
+
+namespace ToDo.Application.Interfaces.Services;
+
+public interface IToDoItemService
+{
+	Task<ToDoItem?> GetByIdAsync(Guid id);
+	Task<IEnumerable<ToDoItem>> GetAllAsync();
+	Task<Guid> AddAsync(ToDoItem toDoItem);
+	Task UpdateAsync(ToDoItem toDoItem);
+	Task DeleteAsync(Guid id);
+	Task<bool> ExistsAsync(Guid id);
+	Task MarkAsDone(Guid id);
+
+	Task SetCompletionPercentageAsync(
+		Guid id,
+		int percentage);
+
+	Task<IEnumerable<ToDoItem>> GetIncomingAsync(IncomingPeriod incomingPeriod);
+}
