c7c06a064941c3edb96c75228fe0da52631f2606
Witold <witold778899@gmail.com>
2025-10-19 11:04:01 +0200

    Configure DI for DbContext, repository, and service layers in API project

--- ToDo.API/Program.cs
+++ ToDo.API/Program.cs
@@ -1,15 +1,29 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using ToDo.Application.Interfaces.Repositories;
+using ToDo.Application.Interfaces.Services;
+using ToDo.Application.Services;
+using ToDo.Infrastructure.Data;
 using ToDo.Infrastructure.Health;
+using ToDo.Infrastructure.Repositories;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IToDoItemRepository, ToDoItemRepository>();
+
 builder.Services.AddHealthChecks()
 	.AddCheck<DatabaseHealthCheck>("custom-sql", HealthStatus.Unhealthy);
+
+builder.Services.AddScoped<IToDoItemService, ToDoItemService>();
 
 WebApplication app = builder.Build();
 
@@ -20,8 +34,10 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
-app.MapHealthChecks("health");
-
 app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.MapHealthChecks("health");
 
 app.Run();
